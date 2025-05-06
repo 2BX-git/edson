e97b0b2e692969c7a99bbf20852fd1b4c8ff2ef6
2BX <140265935+2BX-git@users.noreply.github.com>
2025-05-06 13:09:53 -0300

    Update crm.php

--- crm.php
+++ crm.php
@@ -145,7 +145,7 @@ $result = $conn->query($sql);
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>CRM - Calculador</title>
+    <title>Leads - Irrigação</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
